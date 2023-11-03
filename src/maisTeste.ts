1e5193656918f7b5cd48b5506eb66f80a4d1d6ca
giovansmac <giovani.emp@gmail.com>
2023-11-03 20:14:14 -0300

    forth_commit

--- src/maisTeste.ts
+++ src/maisTeste.ts
@@ -6,175 +6,177 @@ import {getUserEmbedding} from './utils/exporttest'
 import { ChatCompletionMessage } from "openai/resources/chat"
 import { initPrompt } from './utils/initPrompt'
 import { getDFcustomer } from './utils/similaritySrc'
+import{ codeHistory} from './teste'
 import {inatividade} from './teste'
 
 
 
 const simulatedMessage: any = {
-  id: 0, // Um ID único para a mensagem
-  status: "",
-  body: 'Boa noite, tudo certo?', // O corpo da mensagem
-  from: 'userteste5',
-  type: 'chat', // O tipo da mensagem (pode ser 'chat', 'image', 'video', etc.)
-  t: new Date().getTime(), // A data e hora da mensagem como timestamp
-  notifyName: 'Nome do remetente simulado', // O nome do remetente
-  // Outras propriedades que você deseja simular
-  isGroupMsg: false, // Ou true, dependendo do cenário que deseja testar
+    id: 0, // Um ID único para a mensagem
+    status: "",
+    body: 'Boa tarde, tudo certo?', // O corpo da mensagem
+    from: 'userteste7',
+    type: 'chat', // O tipo da mensagem (pode ser 'chat', 'image', 'video', etc.)
+    t: new Date().getTime(), // A data e hora da mensagem como timestamp
+    notifyName: 'Nome do remetente simulado', // O nome do remetente
+    // Outras propriedades que você deseja simular
+    isGroupMsg: false, // Ou true, dependendo do cenário que deseja testar
 };
-
-
 
 // https://wa.me/+5512982754592
 export interface CustomerChat {
-status?: "open" | "closed"| "pending"
-orderCode: string
-chatAt: string
-customer: {
-  name: string
-  phone: string
-}
-messages: ChatCompletionMessage []
-embedding?: number[]
+  status?: "open" | "closed"| "pending"| ""
+  orderCode: string
+  chatAt: string
+  customer: {
+    name: string
+    phone: string
+  }
+  messages: ChatCompletionMessage []
+  embedding?: number[]
 }
 
 export async function completion(
-messages: ChatCompletionMessage[]
+  messages: ChatCompletionMessage[]
 ): Promise<string | undefined> {
-const completion = await openai.chat.completions.create({
-  model: "gpt-3.5-turbo", 
-  temperature: 0,
-  max_tokens: 256,
-  messages,
-})
-console.log(completion.usage?.total_tokens)
-return completion.choices[0].message?.content || undefined
+  const completion = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo", 
+    temperature: 0,
+    max_tokens: 256,
+    messages,
+  })
+  console.log(completion.usage?.total_tokens)
+  return completion.choices[0].message?.content || undefined
 }
+
+
+///tem que colocar essa função dentro do start client quando for subir pra produção
+const tempoEmMilissegundos = 1 * 60 * 1000   
+  setTimeout(async () => {
+  const chat = await inatividade()
+  if (chat.status != ""){
+  console.debug(chat)
+}
+}, tempoEmMilissegundos);
+////////////////
 
 /*create({
-session: "food-gpt",
-disableWelcome: true,
+  session: "food-gpt",
+  disableWelcome: true,
 })
-.then(async (client: Whatsapp) => await start(client))
-.catch((err) => {
-  console.log(err)
-})*/
+  .then(async (client: Whatsapp) => await start(client))
+  .catch((err) => {
+    console.log(err)
+  })*/
 
 //async function start(client: Whatsapp) {
-
-//client.onMessage(async (message: Message) => {
-  async function testeMessage(message: Message) {
-    if (!message.body || message.isGroupMsg) return
-    
-  const customerPhone = `+${message.from.replace("@c.us", "")}`
-  const customerName = message.author
-  let customerKey = `customer:${customerPhone}:chat:`
-  const keys = (await redisset.keys(customerKey+'*')|| null)
-  const tempoEmMilissegundos = 30 * 60 * 1000   
  
+  //client.onMessage(async (message: Message) => {
+    async function testeMessage(message: Message) {
+      if (!message.body || message.isGroupMsg) return
+      
+    const customerPhone = `+${message.from.replace("@c.us", "")}`
+    const customerName = message.author
+    let customerKey = `customer:${customerPhone}:chat:`
+    const keys = (await redisset.keys(customerKey+'*')|| null)
+     
 
-  let lastChat;
-  let orderCode = `#sk-${("00000" + Math.random()).slice(-5)}`
-  let embedding = await getUserEmbedding(orderCode)
-  customerKey = customerKey+orderCode
-  
-  if (message.body.includes('#sk-')) {
-    // Extrair o código do chamado do corpo da mensagem
-    const codeMatch = message.body.match(/#sk-\d+/);
+    let lastChat;
+    let orderCode = `#sk-${("00000" + Math.random()).slice(-5)}`
+    let embedding = await getUserEmbedding(orderCode)
+    customerKey = customerKey+orderCode
     
-    if (codeMatch) {
-      const customerCode = codeMatch[0]; // Obtém o código do chamado
-      customerKey = customerKey+`${customerCode}`; // Crie a chave do histórico
-      console.log(customerKey)
-      // Verifique se há histórico para o código do chamado
-      const chatHistory = await redisset.get(customerKey);
-  
-      if (chatHistory) {
-        // Carregue o histórico da conversa
-        lastChat = JSON.parse(chatHistory);
-        // Defina o status do chat como "aberto"
-        lastChat.status = 'pending';
-        lastChat.orderCode = customerCode;
-        await redisset.set(customerKey, JSON.stringify(lastChat))
-        // Agora você pode trabalhar com o histórico da conversa
-        console.log('Histórico da conversa:', lastChat);
-      } else {
-        console.log('Código de chamado não encontrado.');
-      }
-    }
-  }else {
-  for (const key of keys){
-      let stat: any = await redisset.get(key)
-      stat = JSON.parse(stat)
-      if(stat.status === 'open' || stat.status === 'pending' ){
-          stat.status = 'open'
-          lastChat = stat
-          customerKey = key
-          console.log(lastChat)
-          break;
-    }
-   }
-  } 
-
-  const customerChat: CustomerChat =
-  (lastChat?.status === "open" || lastChat?.status === 'pending')
-    ? (lastChat as CustomerChat)
-      : {
-          status: "open",
-          orderCode,
-          chatAt: new Date().toISOString(),
-          customer: {
-            name: customerName,
-            phone: customerPhone,
-          },
-          messages: [
-            {
-              role: "system",
-              content: initPrompt(orderCode),
-            },
-          ],
-          embedding,
+    if (message.body.includes('#sk-')) {
+      // Extrair o código do chamado do corpo da mensagem
+      const codeMatch = message.body.match(/#sk-\d+/);
+      
+      if (codeMatch) {
+        const customerCode = codeMatch[0]; // Obtém o código do chamado
+        customerKey = customerKey+`${customerCode}`; // Crie a chave do histórico
+        console.log(customerKey)
+        // Verifique se há histórico para o código do chamado
+        const chatHistory = await redisset.get(customerKey);
+    
+        if (chatHistory) {
+          // Carregue o histórico da conversa
+          lastChat = JSON.parse(chatHistory);
+          // Defina o status do chat como "aberto"
+          lastChat.status = 'pending';
+          lastChat.orderCode = customerCode;
+          await redisset.set(customerKey, JSON.stringify(lastChat))
+          // Agora você pode trabalhar com o histórico da conversa
+          console.log('Histórico da conversa:', lastChat);
+        } else {
+          console.log('Código de chamado não encontrado.');
         }
-        
-        
-  console.debug(customerPhone, "👤", message.body)
+      }
+    }else {
+    for (const key of keys){
+        let stat: any = await redisset.get(key)
+        stat = JSON.parse(stat)
+        if(stat.status === 'open' || stat.status === 'pending' ){
+            stat.status = 'open'
+            lastChat = stat
+            customerKey = key
+            console.log(lastChat)
+            break;
+      }
+     }
+    } 
 
+    const customerChat: CustomerChat =
+    (lastChat?.status === "open" || lastChat?.status === 'pending')
+      ? (lastChat as CustomerChat)
+        : {
+            status: "open",
+            orderCode,
+            chatAt: new Date().toISOString(),
+            customer: {
+              name: customerName,
+              phone: customerPhone,
+            },
+            messages: [
+              {
+                role: "system",
+                content: initPrompt(orderCode),
+              },
+            ],
+            embedding,
+          }
+          
+    console.debug(customerPhone, "👤", message.body)
 
-  let res = await resChat(message.body)
-
-  customerChat.messages.push({
-    role: "user",
-    content: res},)
-    
-    
-   const content =
-    (await completion(customerChat.messages)) || "Não entendi..."
+    let res = await resChat(message.body)
 
     customerChat.messages.push({
-      role: "assistant",
-      content,})
+      role: "user",
+      content: res},)
+      
+      
+    let content =
+      (await completion(customerChat.messages)) || "Não entendi..."
 
-      const inativ =  setTimeout(async () => {await inatividade(customerKey)}, tempoEmMilissegundos);
-      if(inativ){
-       customerChat.messages.push({
-         role: "assistant",
-         content: `atendimento com código:${orderCode} encerrado por inatividade, para dar continuidade neste atendimento, digite o código na primeira menssagem. Desde já grato.`},)
+      customerChat.messages.push({
+        role: "assistant",
+        content,})
+
+        content =
+      (await completion(customerChat.messages)) || "Não entendi..."
+
+        console.debug(customerPhone, "🤖", content)
+
+    //await client.sendText(message.from, content)
+  
+    if (customerChat.status === "open" && content.includes(customerChat.orderCode)) {
+        customerChat.status = "closed";
+  
+        console.debug(customerPhone, "📦", content)
+        
+        customerChat.embedding = embedding
       }
 
-  console.debug(customerPhone, "🤖", content)
-
-  //await client.sendText(message.from, content)
-
-  if (customerChat.status === "open" && content.includes(customerChat.orderCode)) {
-      customerChat.status = "closed";
-
-      console.debug(customerPhone, "📦", content)
-      
-      customerChat.embedding = embedding
-    }
-
-  redisset.set(customerKey, JSON.stringify(customerChat))
-}
-testeMessage(simulatedMessage)
-
+    redisset.set(customerKey, JSON.stringify(customerChat))
+  }
+  testeMessage(simulatedMessage)
 
 //}
--- src/teste.ts
+++ src/teste.ts
@@ -1,10 +1,11 @@
 import{getDF} from './utils/similaritySrc'
-import {ask} from './utils/exporttest'
+//import {ask} from './old/exporttest'
 import { queryMessage } from './utils/exporttest'
 import { getDFcustomer } from './utils/similaritySrc'
-import {reviseTextWithGPT3} from './utils/exporttest'
+//import {reviseTextWithGPT3} from './old/exporttest'
 import {stringsRankedByRelatedness} from './utils/similaritySrc'
 import { redisset } from './lib/redis'
+import { CustomerChat } from './maisTeste'
 
 
 export async function resChat(query:string){
@@ -14,11 +15,13 @@ const message  = await queryMessage(query, df, 'gpt-3.5-turbo', 4096 - 500)
 //let keys: any =(await redisset.keys('customer:+userteste4:chat:*'))||null
 //const dfc = await getDFcustomer('customer:+userteste4:chat:')
 //await queryMessage(query, dfc, 'gpt-3.5-turbo', 4096 - 500)
+console.log(message)
 return message
 }
-
 //resChat('EU tenho uma chamado aberto #sk-97392')
-/*export async function codeHistory(message:string, customer:string) {
+
+
+export async function codeHistory(message:string, customer:string) {
 let  customerkey = customer
 const dfc = await getDFcustomer(customerkey)
 console.log(dfc)
@@ -26,26 +29,42 @@ const similarity = await stringsRankedByRelatedness(message, dfc, 1)
 customerkey = customerkey+similarity[0][0]
 console.log(customerkey)
 return customerkey
-}*/
+}
 //codeHistory('#sk-93177', 'customer:+userteste4:chat:' )
 
-export async function inatividade(key: string) {
-    let chat = JSON.parse((await redisset.get(key)) || "{}")
-        let changestatus: boolean = false
-        let chatDate = new Date(chat.chatAt);
-        // Data e hora atual
-        const dataHoraAtual = new Date();
-        // Calcule a diferença em milissegundos
-        const diferencaEmMilissegundos = dataHoraAtual.getTime() - chatDate.getTime();
-        // Calcule a diferença em minutos
-        const diferencaEmMinutos = Math.floor(diferencaEmMilissegundos / (1000 * 60));
-        // Verifique se a diferença em minutos é maior que 30
-        if (diferencaEmMinutos > 30) {
-            changestatus = true
-        } else {
-          console.log('Status não atualizado.');
-        }
-        return changestatus
+ export async function inatividade (){
+    let customerChat: CustomerChat = {
+        orderCode: '',
+        chatAt: '',
+        customer: {
+            name: '',
+            phone: ''
+        },
+        messages: []
     }
-
-
+    const keys = await redisset.keys('customer:*')
+    console.log(keys)
+    for(const key of keys){
+    customerChat = JSON.parse((await redisset.get(key))||'{}')
+    let chatDate = new Date(customerChat.chatAt);
+    // Data e hora atual
+    const dataHoraAtual = new Date();
+    // Calcule a diferença em milissegundos
+    const diferencaEmMilissegundos = dataHoraAtual.getTime() - chatDate.getTime();
+    // Calcule a diferença em minutos
+    const diferencaEmMinutos = Math.floor(diferencaEmMilissegundos / (1000 * 1));
+    // Verifique se a diferença em minutos é maior que 30
+    if (diferencaEmMinutos > 1 && customerChat.status === 'open' ) {
+       customerChat.status = 'closed'
+        customerChat.messages.push({
+            role: "assistant",
+            content:  `atendimento encerrado por inatividade, o código do atendimento é: ${customerChat.orderCode}. Caso queira dar continuidade a este atendimento, digite este código para o atendente virtual. Desde já grato.`,})
+            await redisset.set(key, JSON.stringify(customerChat))
+            console.log('Status atualizado.');
+    } else {
+      console.log('Status não atualizado.');
+      customerChat = customerChat
+    }
+}
+return customerChat
+}
